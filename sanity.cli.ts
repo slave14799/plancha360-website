c02abc02e54e5b6889811cbd1a8dec93e2d2cda9
slave14799 <slave14799@gmail.com>
2026-06-12 14:08:04 +0200

    chore: add sanity appId and styled-components

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+    api: {
+        projectId: 'aa47lzwl',
+        dataset: 'production',
+    },
+    deployment: {
+        appId: 'emfb547yp8c6kyuidxug8qzd',
+    },
+})
